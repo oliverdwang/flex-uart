d1de63c1ae6812dfb81837aba3b2ce0b45adcfdd
David Xie <davidx@andrew.cmu.edu>
2022-03-01 16:16:58 -0500

    initial top file

--- rtl/uart-top.sv
+++ rtl/uart-top.sv
@@ -17,11 +17,29 @@ module uart
     output logic       tx_data_ready,
     // Host receive interface
     input  logic       rx_data_ready,
+    input  logic       rx_framing_err_clr,
     output logic       rx_data_valid,
     output logic [7:0] rx_data,
+    output logic       rx_overrun,
+    output logic       rx_framing_err
   );
 
-  
+  receiver uart_recv(.clk,
+                     .rst_n,
+                     .raw_rx_bitstream(rx_datastream),
+                     .host_ready(rx_data_ready),
+                     .clear_framing_err(rx_framing_err_clr),
+                     .rx_data,
+                     .rx_data_valid,
+                     .framing_err(rx_framing_err),
+                     .overrun(rx_overrun));
+
+  transmitter uart_write(.clk,
+                         .rst_n,
+                         .tx_data_valid,
+                         .tx_data,
+                         .tx_data_ready,
+                         .tx_serial_out(tx_datastream));
   
 
 endmodule: uart
